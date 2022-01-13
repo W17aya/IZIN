3e04f08e215c4d8667b90b3ed3f8c549c46e8e77
Eko Wijaya <ekowijayawindows@gmail.com>
2022-01-13 23:53:59 +0700

    napa dah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    JANCOK KON
                 </div>
 
                 <div class="links">
